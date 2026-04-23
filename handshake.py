4e8cd85e3722a372e21d93ac7928c42dc04016f1
Ukbenoz <149002105+Ukbenoz@users.noreply.github.com>
2026-04-22 21:00:45 -0400

    WORKING
    gogogo

---
+++ handshake.py
@@ -0,0 +1,21 @@
+import struct
+
+# This function will build the 32-byte message.
+def create_handshake(peer_id):
+    # '18s' = 18-byte string
+    # '10x' = 10 padding bytes (zeros)
+    # 'I' = 4-byte unsigned integer
+    header = b"P2PFILESHARINGPROJ"
+    
+    # We use the header to create the full message here.
+    return struct.pack(">18s10xI", header, peer_id)
+
+
+# This function helps check if the receiving header is valid.
+def verify_handshake(received_bytes, expected_peer_id):
+    header, peer_id = struct.unpack(">18s10xI", received_bytes)
+    print(f"[Handshake] Got header={header}, peer_id={peer_id}, expected={expected_peer_id}")
+    if header == b"P2PFILESHARINGPROJ" and peer_id == expected_peer_id:
+        return True
+    return False
+
